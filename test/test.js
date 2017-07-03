d947f2ef43c8e269b3ba11214a5e5af4e45667fd
dror rash <drorrash@drors-MacBook-Pro.local>
2017-07-03 17:34:20 +0300

    Initial ealease

---
+++ test/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var expect = require('chai').expect;
+var numFormatter = require('../index.js');
+
+describe('#numFormatter', function() {
+  it('should convert single digits', function() {
+    var result = numFormatter(1);
+    expect(result).to.equal('1');
+  });
+  it('should convert double digits', function() {
+    var result = numFormatter(12);
+    expect(result).to.equal('12');
+  });
+  it('shouldconvert triple digits', function() {
+    var result = numFormatter(123);
+    expect(result).to.equal('123');
+  });
+});
